347e70b25e58be71a11a7ffb74ebdf62af49ab12
Shruthi M <111205197+shru0405@users.noreply.github.com>
2023-01-25 03:11:04 -0500

    Appl.Py

---
+++ Week_4/Appl.py
@@ -0,0 +1,18 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+application = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@application.route('/')
+def home():
+    return render_template('index.html')
+
+@application.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
