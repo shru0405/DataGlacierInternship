400eefa380c23c621e34c59b8987e294bf8ff9f7
Shruthi M <111205197+shru0405@users.noreply.github.com>
2023-02-03 14:36:41 -0500

    Add files via upload

---
+++ Week_5/FlaskDeploy/Flask_Project/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FlaskDeploy',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='None',
+    author='Shruthi',
+    author_email='',
+    description='A flask-app '
+)
